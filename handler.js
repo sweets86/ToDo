bcef1528fc0e43513eb17aaa6bbe23f8f2281adc
yanica svensson <sweets86_@hotmail.com>
2019-11-01 10:17:24 +0100

    init

--- handler.js
+++ handler.js
@@ -1,1 +1,17 @@
 
+
+var username = "Yanica"
+var password = "Svensson"
+
+function validateForm() {
+    var usernameInput = document.getElementById("username").value
+    var passwordInput = document.getElementById("password").value
+    var main = document.getElementById("main")
+    
+    if (usernameInput == username && passwordInput == password) {
+        main.innerText = username + password
+        
+    } else {
+        main.innerText = ("Har du glömt ditt lösenord?")
+    }
+}
